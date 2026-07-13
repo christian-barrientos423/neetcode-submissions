404535212e0fc35cf9833b1a4f99431238940989
Christian Barrientos <ccruz@bandwidth.com>
2026-07-12 21:34:02 -0400

    Add: binary-search - submission-5

---
+++ Data Structures & Algorithms/binary-search/submission-5.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int mid = nums.length/2;
+        int start = 0;
+        int end = nums.length - 1;
+     
+        
+        while(start <= end) {
+            mid = (end-start)/2 + start;
+            int current = nums[mid];
+            if(current == target) {
+                return mid;
+            } else if(current > target) {
+                end = mid - 1;
+                
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
